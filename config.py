d1f25f2a3863ad9ea2d7f46ed890fa67a9e50861
Магомед Магомед <hitgas123@iMac-Magomed-Magomed.local>
2022-06-15 12:02:13 +0300

    new project baza

---
+++ config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+
+
+class Config:
+    JSON_AS_ASCII = False
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR / 'main.db'}"
+    SQLALCHEMY_TRACK_MAODIFICATIONS = False
+    DEBUG = True
+    PORT = 5000
+    SECRET_KEY = 'supeR secret KeyS'
+    TEST_DATABASE_URI = F"SQLITE:///{BASE_DIR / 'main.db'}"
