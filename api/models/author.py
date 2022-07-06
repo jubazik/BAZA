e0c842cd15fcc3a13fc64e5fd7037c9cbf2e979c
zizik <hitgas12@mail.ru>
2022-07-06 17:28:34 +0300

    room сделал не обязательным аргументом

--- api/models/author.py
+++ api/models/author.py
@@ -5,11 +5,11 @@ from api import db, date
 class NameCard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, default=date)
-    name = db.Column(db.String(32), unique=True)
+    name = db.Column(db.String(32), nullable=False, serve_default='Default')
     room = db.Column(db.Integer, unique=True)
     sun = db.relationship('Payments', backref='author', lazy='dinamic')
 
-    def __init__(self, name, room):
+    def __init__(self, name, room = 'нет записи'):
         self.name = name
         self.room = room
 
