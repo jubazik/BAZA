4531aa238c7524856c459ce53c279d2f42941fe1
zizik <hitgas12@mail.ru>
2022-07-09 11:54:18 +0300

    commit

--- api/resources/author.py
+++ api/resources/author.py
@@ -12,43 +12,26 @@ class NameCardResource(Resource):
         return author.to_dict()
 
 
-    def put(self, author_id):
-        # parser = reqparse.RequstParser()
-        # parser.add_argument('name', required=True)
-        # author_data = parser.parse_args()
-        author_data = request.json
-        author = NameCard.query.get(author_id)
-        if author is None:
-            return {"Error": f"Author id={author_id} not found"}, 404
-        for key, value in author_data.items:
-            setattr(author, key, value)
-
-        # author.name = author_data['name']
 
     """PUT запрос на изменение контрагенты по id  """
     def put(self, author_id):
         """не реализован!!!"""
-        # parser = reqparse.RequstParser()
-        # parser.add_argument('name', required=True)
-        # author_data = parser.parse_args()
         new_author = request.json
         author = NameCard.query.get(author_id)
         if author is None:
             return {"Error": f"Author id={author_id} not found"}, 404
-        # author.name = author_data['name']
         for key, value in new_author.items():
             setattr(author, key, value)
-
         db.session.commit()
-        return jsonify( author.to_dict())
+        return jsonify(author.to_dict())
 
         """delete удаление контрагента по id """
+
     def delete(self, author_id):
         author = NameCard.query.get(author_id)
         db.session.delete(author)
         db.session.commit()
         return f"{author.to_dict()}, успешно удалено"
-        # raise NatImplemented("Метод не реализован")
 
 
 
@@ -62,9 +45,7 @@ class NameListCardResource(Resource):
         return authors_list, 200
 
     def post(self):
-        # parser = reqparse.RequstParser()
-        # parser.add_argument('name', required=True)
-        # authors_data = parser.parse_args()
+  
         author_data = request.json
         author = NameCard(**author_data)
         db.session.add(author)
